1559d4716a9aa38907e83eeee974b1b16250eb79
Leonardo Flores <scrowws@gmail.com>
2023-11-28 19:48:43 -0300

    js to html

---
+++ aula4/index.js
@@ -0,0 +1,10 @@
+// Para criar comentarios no java script e necessario 
+// incluir duas barras na frente da linha
+console.log('Comentarios'); //podendo ser utilizado em qualquer parte do codigo
+/* 
+
+    Dessa forma voce consegui comentar em bloco
+    nao sendo necessario incluir linha por linha
+    facilitando para auxiliar na documentacao do software
+
+*/
