72c93ff194d2b3ce1dc7ba26d16794ef088236f7
Leonardo Flores <scrowws@gmail.com>
2023-11-29 18:41:55 -0300

    new

---
+++ 6/index.js
@@ -0,0 +1,11 @@
+console.log('Hello world again, cause i started a better course :D');
+
+window.alert('Nice to see you here');
+
+//You can comment like this
+/*
+    Or you can comment like this :O
+*/
+
+document.getElementById("myh1").textContent = 'Hello';
+document.getElementById("myp").textContent = 'i like you <3';
