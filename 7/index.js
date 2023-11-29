780bdbd4a9d3090612d3b0d90736b02a8530c336
Leonardo Flores <scrowws@gmail.com>
2023-11-29 19:10:11 -0300

    Trying variables in JS

---
+++ 7/index.js
@@ -0,0 +1,39 @@
+/*
+
+Some vars
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is ${gpa}`);
+
+let firstName = "Leonardo";
+let food = "bread";
+let email= "scrowws@gmail.com"
+
+console.log(typeof firstName);
+console.log(`My name is ${firstName}`);
+console.log(`My favorite food is ${food}`);
+console.log(`My email is ${email}`);
+
+
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(`Scrow is online: ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Erolled: ${isStudent}`);
+
+*/
+
+let fullName = "Leonardo Santos";
+let age = 23;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
