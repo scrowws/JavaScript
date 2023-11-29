dd7c024f5f748036f8eae9ed9dac028225d06768
Leonardo Flores <scrowws@gmail.com>
2023-11-29 19:51:36 -0300

    Type conversion

--- 8/index.js
+++ 8/index.js
@@ -4,3 +4,14 @@ document.getElementById("button").onclick = function(){
     username = document.getElementById("nick").value;
     document.getElementById("h1").textContent = `Hello ${username}`;
 }
+
+
+let x = 10, y = 10, z = 10;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
